b1165e4e0787cd83ac24b7b9deec2ec03360706f
aglegaspi <lex83nyc@gmail.com>
2020-09-03 13:24:40 -0400

    project created

---
+++ Dicee_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee_SwiftUI
+//
+//  Created by Alex 6.1 on 9/2/20.
+//  Copyright © 2020 aglegaspi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
